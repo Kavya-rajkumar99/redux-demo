7b4af3e61181e9757c0fc7ec849671ba18938b43
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-07-30 13:45:13 +0530

    mapStateToProps, mapDispatchToProps and connect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import {CakeContainer} from './components/CakeContainer';
 import { IceCreamContainer } from './components/IceCreamContainer';
+import ItemContainer from './components/ItemContainer';
 import {Provider} from 'react-redux';
 import {store} from './Redux/store'
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+      <ItemContainer />
+      <ItemContainer cake="cake"/>
       <CakeContainer />
       <IceCreamContainer />
     </div>
